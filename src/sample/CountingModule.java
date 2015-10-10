74f07eb557e91300fe557c8c6b67e82c762bfbd2
nowb <nowickib@ee.pw.edu.pl>
2015-10-10 16:29:04 +0200

    Merge branch 'master' of C:\Users\bartosz\IdeaProjects\PartyPlanner with conflicts.

--- src/sample/CountingModule.java
+++ src/sample/CountingModule.java
@@ -1,7 +1,5 @@
-package sample;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.StringJoiner;
 
 /**
  * Created by Marcin on 2015-10-10.
@@ -11,69 +9,59 @@ public class CountingModule {
     private double time;
     private int numberOfMen;
     private int numberOfWomen;
-    private String typeOfAlcochol;
-    private String typeOfParty;
+    private int typeOfAlcochol;
+    private double ml;
     private double liters;
     private double price = 0;
     private double prifferedPrice;
 
-    public void setNumberOfMen(int numberOfMen) {
-        this.numberOfMen = numberOfMen;
-    }
 
-    public void setNumberOfWomen(int numberOfWomen) {
-        this.numberOfWomen = numberOfWomen;
-    }
 
-    public void setTypeOfAlcochol(String typeOfAlcochol) {
-        this.typeOfAlcochol = typeOfAlcochol;
-    }
-
-    public void setTypeOfParty(String typeOfParty) {
-        this.typeOfParty = typeOfParty;
-    }
-
-    private double countGrams(){
+    private void countGrams(){
         double promilFactor = -(double)time * (-0.12) + promile + 0.12;
         double cleanAlcolInMl = promilFactor * 70;
-        return cleanAlcolInMl * (numberOfMen + 0.5 * numberOfWomen);
+        ml =  cleanAlcolInMl * (numberOfMen + 0.5 * numberOfWomen);
     }
 
-    private void chooseTypeOfParty(String typeOfParty){
-        switch(typeOfParty){
-            case "Kulturalna prywatka":
+    private void chooseTypeOfParty(int PartyType){
+        switch(PartyType){
+            case 1:
                 promile = 0.5;
                 break;
-            case "Klasyczna domÃ³wka":
+            case 2:
                 promile = 1.35;
                 break;
-            case "Mordownia":
+            case 3:
                 promile = 2.5;
                 break;
         }
     }
 
-    private double chooseAlco(String typeOfAlcochol) {
+    private int chooseAlco(int typeOfAlcochol) {
         switch (typeOfAlcochol){
-            case "Piwo":
+            case 2:
+                price = 30;
+                this.typeOfAlcochol = 2;
+                return 12;
+            case 1:
                 price = 5;
-                return 0.05;
-            case "Wino":
+                this.typeOfAlcochol = 1;
+                return 5;
+            case 4:
                 price = 30;
-                return 0.12;
-            case "Drinki":
-                price = 40;
-                return 0.5;
-            case "WÃ³dka":
-                price = 30;
-                return 0.38;
+                this.typeOfAlcochol = 4;
+                return 38;
+            case 3:
+                price = 5;
+                this.typeOfAlcochol = 3;
+                return 20;
             default:
                 return 0;
         }
     }
 
-    private void countLiters(String typeOfAlcochol){
-        liters =  (countGrams() / 1000)/chooseAlco(typeOfAlcochol);
+    private void countLiters(){
+        liters =  (ml / 10)/chooseAlco(typeOfAlcochol);
     }
 
     private double price(){
@@ -87,21 +75,21 @@ public class CountingModule {
         if(price()>10*prifferedPrice)
             return "Brakuje Ci kupe kasy";
         else if(price()>5*prifferedPrice)
-            return "Nie bÄ…dÅº Å¼yd dorzuÄ‡ trochÄ™";
+            return "Nie b¹dŸ ¿yd dorzuæ trochê";
         else if(price()>2*prifferedPrice)
-            return "MaÅ‚o kapusty!";
+            return "Ma³o kapusty!";
         else if(price()>prifferedPrice)
-            return "BrakÅ‚o trochÄ™ :/";
+            return "Brak³o trochê :/";
         else if(10*price()<prifferedPrice)
-            return "Szykuje siÄ™ gruba impreza, wziÄ…Å‚bym 10 razy tyle :P";
+            return "Szykuje siê gruba impreza, wzi¹³bym 10 razy tyle :P";
         else if(5*price()<prifferedPrice)
-            return "WeÅº wiÄ™cej, nie zmarnuje siÄ™ :D";
+            return "WeŸ wiêcej, nie zmarnuje siê :D";
         else if(2*price()<prifferedPrice)
-            return "MoÅ¼na szaleÄ‡ ;)";
+            return "Mo¿na szaleæ ;)";
         else if(price()==prifferedPrice)
             return "W sam raz :D";
         else
-            return "Jeszcze pare moÅ¼na wziÄ…Ä‡";
+            return "Jeszcze pare mo¿na wzi¹æ";
     }
 
 
@@ -111,13 +99,15 @@ public class CountingModule {
         countingModule.numberOfMen = 2;
         countingModule.numberOfWomen = 2;
         countingModule.time = 3;
-        countingModule.chooseTypeOfParty("Mordownia");
-        countingModule.countLiters("Piwo");
+        countingModule.chooseTypeOfParty(2);
+        countingModule.chooseAlco(1);
+        countingModule.countGrams();
+        countingModule.countLiters();
         System.out.println(countingModule.liters);
         System.out.println(countingModule.price());
         System.out.println();
 
-        countingModule.prifferedPrice = 200;
+        countingModule.prifferedPrice = 1000.0;
         System.out.println(countingModule.comparePrices());
 
 
