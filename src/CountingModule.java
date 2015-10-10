8869349e2b902107f6585f672978a3488a9db29f
wolszczc <czarekwolszczak@wp.pl>
2015-10-10 13:39:37 +0200

    Zmiany w ContingModule.java

--- src/CountingModule.java
+++ src/CountingModule.java
@@ -9,7 +9,7 @@ public class CountingModule {
     private double time;
     private int numberOfMen;
     private int numberOfWomen;
-    private String PartyType;
+    private int typeOfAlcochol;
     private double ml;
     private double liters;
     private double price = 0;
@@ -22,19 +22,37 @@ public class CountingModule {
         ml =  cleanAlcolInMl * (numberOfMen + 0.5 * numberOfWomen);
     }
 
-    private int chooseAlco(String PartyType) {
-        switch (PartyType){
-            case "banquet":
+    private void chooseTypeOfParty(int PartyType){
+        switch(PartyType){
+            case 1:
+                promile = 0.5;
+                break;
+            case 2:
+                promile = 1.35;
+                break;
+            case 3:
+                promile = 2.5;
+                break;
+        }
+    }
+
+    private int chooseAlco(int typeOfAlcochol) {
+        switch (typeOfAlcochol){
+            case 2:
                 price = 30;
+                this.typeOfAlcochol = 2;
                 return 12;
-            case "meeting":
+            case 1:
                 price = 5;
+                this.typeOfAlcochol = 1;
                 return 5;
-            case "home":
+            case 4:
                 price = 30;
+                this.typeOfAlcochol = 4;
                 return 38;
-            case "outdoors":
+            case 3:
                 price = 5;
+                this.typeOfAlcochol = 3;
                 return 20;
             default:
                 return 0;
@@ -42,12 +60,12 @@ public class CountingModule {
     }
 
     private void countLiters(){
-        liters =  (ml / 10)/chooseAlco(PartyType);
+        liters =  (ml / 10)/chooseAlco(typeOfAlcochol);
     }
 
     private double price(){
         if(price == 0){
-            chooseAlco(PartyType);
+            chooseAlco(typeOfAlcochol);
         }
         return liters * price;
     }
@@ -57,8 +75,8 @@ public class CountingModule {
         countingModule.numberOfMen = 2;
         countingModule.numberOfWomen = 2;
         countingModule.time = 3;
-        countingModule.promile = 0.5;
-        countingModule.PartyType = "meeting";
+        countingModule.chooseTypeOfParty(2);
+        countingModule.chooseAlco(1);
         countingModule.countGrams();
         countingModule.countLiters();
         System.out.println(countingModule.liters);
