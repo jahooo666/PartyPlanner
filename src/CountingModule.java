de687aaeb3538d7e8150195e225ff3030473a1ea
Marcin Matłacz <dlugopix@gmail.com>
2015-10-10 12:58:08 +0200

    Liczy wszystko ale źle

--- src/CountingModule.java
+++ src/CountingModule.java
@@ -11,31 +11,61 @@ public class CountingModule {
     private int numberOfWomen;
     private String PartyType;
     private double grams;
+    private double liters;
+    private double price = 0;
+
 
 
     private void countGrams(){
-        double gramsPerPerson = 0;
-        /*Miejsce na obliczenia*/
-
-        grams =  gramsPerPerson * (numberOfMen + 0.5 * numberOfWomen);
+        double promilFactor = -(double)time * (-0.12) + promile + 0.12;
+        double cleanAlcolInMl = promilFactor * 70;
+        grams =  cleanAlcolInMl * (numberOfMen + 0.5 * numberOfWomen);
     }
 
-    private int chooseAlco(String PartyType){
+    private int chooseAlco(String PartyType) {
         switch (PartyType){
             case "banquet":
+                price = 30;
                 return 12;
             case "meeting":
+                price = 5;
                 return 5;
             case "home":
+                price = 30;
                 return 38;
             case "outdoors":
+                price = 5;
                 return 20;
             default:
-                return 5;
+                return 0;
         }
     }
 
-    private double countLiters(){
-        return grams/chooseAlco(PartyType);
+    private void countLiters(){
+        liters =  grams/chooseAlco(PartyType);
+    }
+
+    private double price(){
+        if(price == 0){
+            chooseAlco(PartyType);
+        }
+        return liters * price;
+    }
+
+    public static void main(String[] args) {
+        CountingModule countingModule = new CountingModule();
+        countingModule.numberOfMen = 1;
+        countingModule.numberOfWomen = 1;
+        countingModule.time = 3;
+        countingModule.promile = 1;
+        countingModule.PartyType = "meeting";
+        countingModule.countGrams();
+        countingModule.countLiters();
+        System.out.println(countingModule.liters);
+        System.out.println(countingModule.price());
+        System.out.println();
+
+
+
     }
 }
