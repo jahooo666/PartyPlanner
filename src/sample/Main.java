2f385dcb0d3a5bf70fe1ba4bb411131565a4fe76
jahooo <okraskaj@ee.pw.edu.pl>
2015-10-10 12:32:17 +0200

    Przykładowy element dodany do layoutu- jak narazie jeden przycisk

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,17 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    Stage window;
+    BorderPane layout;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Party Planner");
+        window = primaryStage;
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        window.setTitle("Party Planner");
 
-        primaryStage.setScene(new Scene(root, 1000, 600));
-        primaryStage.show();
+
+        VBox vbox=new VBox();
+        vbox.getChildren().add(new Button("przycisk"));
+        Scene scene = new Scene(vbox,1000, 600);
+        window.setScene(scene);
+        window.show();
+
     }
 
 
